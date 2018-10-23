01b24ff56cc8893c9508b7bb245b288abd623150
Amar <athondapu@reputation.com>
2018-10-23 13:34:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { Route, Switch, Redirect } from "react-router-dom";
+import LoginForm from './components/loginForm';
 
-class App extends Component {
+class App extends React.Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <main className="container">
+          <Switch>
+            <Route path="/login" component={LoginForm} />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
---
+++ src/components/loginForm.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
+import auth from "../services/authService";
+
+class LoginForm extends Form {
+  state = {
+    data: { username: "", password: "" },
+    errors: {}
+  };
+
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
+  };
+
+  doSubmit = async () => {
+    console.log("In Do Submit Method: ");
+    try {
+      const { data } = this.state;
+      await auth.login(data.username, data.password);
+      const { state } = this.props.location;
+      window.location = state ? state.from.pathname : "/";
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        const { data } = ex.response;
+        errors[data.field] = data.message;
+        this.setState({ errors });
+      }
+    }
+  };
+
+  render() {
+    // if (auth.getCurrentUser()) return <Redirect to="/" />;
+    return (
+      <div className="row example-wrapper">
+        <div className="col-xs-12 col-sm-6 offset-sm-3 example-col">
+          <div className="card">
+            <div className="card-block">
+              <form className="k-form" onSubmit={this.handleSubmit}>
+                <fieldset>
+                  <legend>Login Form:</legend>
+                  {this.rendetInput("username", "Username")}
+                  {this.rendetInput("password", "Password", "password")}
+                </fieldset>
+                {this.renderButton("Login")}
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
