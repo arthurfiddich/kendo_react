2458eed00042b17dd75d03f59ff8c4b52914998e
Amar <athondapu@reputation.com>
2018-10-24 09:19:03 +0530

    Committing Grid Changes...

--- src/components/ticketsTable.jsx
+++ src/components/ticketsTable.jsx
@@ -3,34 +3,21 @@ import { Grid, GridColumn as Column } from '@progress/kendo-react-grid';
 import TableHeader from './common/tableHeader';
 
 class TicketsTable extends Component {
-    // columns = [
-    //     { path: "ticketId", label: "Ticket ID" },
-    //     { path: "product", label: "Product" },
-    //     { path: "status", label: "Status" },
-    //     { path: "openDate", label: "Open Date" },
-    //     { path: "messages", label: "Messages" },
-    //     { path: "progress", label: "Progress" },
-    //     { path: "payment", label: "Payment" },
-    //     { path: "estimation", label: "Estimation" },
-    //     { path: "techClose", label: "Tech Close" },
-    //     { path: "closeDate", label: "Close Date" },
-    //     { path: "rating", label: "Rating" },
-    //     { path: "actions", label: "Actions" },
-    // ];
-
     constructor(props) {
         super(props);
         this.state = {
-            data: this.createRandomData(50),
-            skip: 0
+            data: this.createRandomData(5000),
+            skip: 0,
+            take: 10
         };
         this.pageChange = this.pageChange.bind(this);
     }
 
     pageChange(event) {
+        console.log("Event: ", event);
         this.setState({
-            orders: this.state.orders,
-            skip: event.page.skip
+            skip: event.page.skip,
+            take: event.page.take
         });
     }
 
@@ -40,36 +27,58 @@ class TicketsTable extends Component {
             lastNames = ['Davolio', 'Fuller', 'Leverling', 'Peacock', 'Buchanan', 'Suyama', 'King', 'Callahan', 'Dodsworth', 'White'],
             cities = ['Seattle', 'Tacoma', 'Kirkland', 'Redmond', 'London', 'Philadelphia', 'New York', 'Seattle', 'London', 'Boston'],
             titles = ['Accountant', 'Vice President, Sales', 'Sales Representative', 'Technical Support', 'Sales Manager', 'Web Designer',
-                'Software Developer'];
+                'Software Developer'],
+            statuses = ['Success', 'Failure'],
+            openDates = ['2018-10-22', '2018-10-23', '2018-10-20', '2018-10-21', '2018-10-19', '2018-10-18'],
+            progresses = ['Grooming', 'InProgress', 'Engineering Ready', 'QA', 'Engineering Done', 'Product Verification', 'Success'],
+            payments = ['Paypal', 'Credit Card Payment', 'Debit Card Payment'],
+            estimations = ['1Billion', '2Billion', '3Billion', '4Billion', '5Billion', '6Billion', '7Billion'],
+            ratings = ['1Star', '2Star', '3Star', '4Star', '5Star'];
+
 
         return Array(count).fill({}).map((_, idx) => ({
-            id: idx + 1,
-            firstName: firstNames[Math.floor(Math.random() * firstNames.length)],
-            lastName: lastNames[Math.floor(Math.random() * lastNames.length)],
-            city: cities[Math.floor(Math.random() * cities.length)],
-            title: titles[Math.floor(Math.random() * titles.length)]
+            ticketId: idx + 1,
+            product: firstNames[Math.floor(Math.random() * firstNames.length)],
+            status: statuses[Math.floor(Math.random() * statuses.length)],
+            openDate: openDates[Math.floor(Math.random() * openDates.length)],
+            messages: titles[Math.floor(Math.random() * titles.length)],
+            progress: progresses[Math.floor(Math.random() * progresses.length)],
+            payment: payments[Math.floor(Math.random() * payments.length)],
+            estimation: estimations[Math.floor(Math.random() * estimations.length)],
+            techClose: lastNames[Math.floor(Math.random() * lastNames.length)],
+            closeDate: openDates[Math.floor(Math.random() * openDates.length)],
+            rating: ratings[Math.floor(Math.random() * ratings.length)],
+            actions: cities[Math.floor(Math.random() * cities.length)],
         }));
     }
     render() {
-        const { data, skip } = this.state;
+        const { data, skip, take } = this.state;
         console.log('Columns: ', this.columns);
         return (
+            // scrollable={'virtual'}
             <Grid
-                style={{ height: '440px' }}
+                style={{ height: '600px' }}
                 rowHeight={40}
-                data={data.slice(skip, skip + 20)}
+                data={data.slice(skip, take + skip)}
                 pageSize={20}
                 total={data.length}
                 skip={skip}
-
-                scrollable={'virtual'}
+                take={take}
+                pageable={true}
                 onPageChange={this.pageChange}
             >
-                <Column field="id" title="ID" width="70px" />
-                <Column field="firstName" title="First Name" />
-                <Column field="lastName" title="Last Name" />
-                <Column field="city" title="City" width="120px" />
-                <Column field="title" title="Title" width="200px" />
+                <Column field="ticketId" title="Ticket ID" />
+                <Column field="product" title="Product" />
+                <Column field="status" title="Status" />
+                <Column field="openDate" title="Open Date" />
+                <Column field="messages" title="Messages" />
+                <Column field="progress" title="Progress" />
+                <Column field="payment" title="Payment" />
+                <Column field="estimation" title="Estimation" />
+                <Column field="techClose" title="Tech Close" />
+                <Column field="closeDate" title="Close Date" />
+                <Column field="rating" title="Rating" />
+                <Column field="actions" title="Actions" />
             </Grid>
         );
     }
